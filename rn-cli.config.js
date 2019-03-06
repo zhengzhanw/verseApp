1db27a288ab5f7d5c1a2b02479a2e0e1c03bd501
ZHENG\Administrator <zhengxuanhz@163.com>
2019-03-06 23:13:25 +0800

    '初始化项目'

---
+++ rn-cli.config.js
@@ -0,0 +1,21 @@
+const { getDefaultConfig } = require("metro-config");
+
+module.exports = (async() => {
+    const {
+        resolver: { sourceExts }
+    } = await getDefaultConfig();
+    return {
+        transformer: {
+            babelTransformerPath: require.resolve("./vueTransformerPlugin.js")
+        },
+        resolver: {
+            sourceExts: [...sourceExts, "vue"]
+        },
+        getTransformModulePath() {
+            return require.resolve("./vueTransformerPlugin.js");
+        },
+        getSourceExts() {
+            return ["vue"];
+        }
+    };
+})();
